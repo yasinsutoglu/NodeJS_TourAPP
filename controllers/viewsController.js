aadbd366358d49a2c86dafe7d3589aa86e7b6689
Yasin Sutoglu <ysutoglu@gmail.com>
2023-01-31 08:44:26 +0300

    building tour details page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,14 @@ exports.getOverview = catchAsync(async (req, res,next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async(req, res) => {
+  //1) get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({slug: req.params.slug}).populate({path: 'reviews' , fields:'review rating user'})
+  //2) Build template
+  
+  //3) Render template using data from step 1
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour:tour
   });
-};
+});
