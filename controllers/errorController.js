31bc102e79ab3645da013aaa5d85d80e7687f42f
Yasin Sutoglu <ysutoglu@gmail.com>
2023-02-01 05:18:25 +0300

    errorPage done

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -35,33 +35,60 @@ const handleValidationErrorDB = (err) => {
 };
 
 // Handling errors during development
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
-// Handling errors during production
-const sendErrorProd = (err, res) => {
-  //Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
+const sendErrorDev = (err, req, res) => {
+  // API
+  if(req.originalUrl.startsWith('/api')){
+    return res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
-    });
-    //Programming or other unknown error: don't leak error details
-  } else {
-    // 1) Log error
-    console.error('ERROR!', err);
-    // 2) Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong',
+      stack: err.stack,
     });
   }
+  //RENDERED WEBSITE
+    return res.status(err.statusCode).render('error' , {
+      title:'Something went wrong!',
+      msg: err.message
+    })
+ };
+
+// Handling errors during production
+const sendErrorProd = (err, req, res) => {
+  //API
+  if(req.originalUrl.startsWith('/api')){
+    //Operational, trusted error: send message to client
+    if (err.isOperational) {
+     return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    } 
+    //Programming or other unknown error: don't leak error details
+      // 1) Log error
+      console.error('ERROR!', err);
+      // 2) Send generic message
+     return res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong',
+      });
+    
+  }
+
+  // RENDERED WEBSITE
+    //Operational, trusted error: send message to client
+  if(err.isOperational){
+     return res.status(err.statusCode).render('error', {
+       title: 'Something went wrong!',
+       msg: err.message,
+     });
+    } 
+    //Programming or other unknown error: don't leak error details
+    console.error('ERROR!', err);
+      // Send generic message
+      return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: 'Please try again later.',
+      }); 
 };
 
 //! globalErrorHandler default export edildi
@@ -70,9 +97,10 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err,req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err }; // deep copy
+    error.message = err.message;
 
     if (error.name === 'CastError') {
       error = handleCastErrorDB(error);
@@ -90,6 +118,6 @@ module.exports = (err, req, res, next) => {
       error = handleJWTExpiredError();
     }
 
-    sendErrorProd(error, res);
+    sendErrorProd(error,req, res);
   }
 };
