4412f8e3f307875e03664daf8529289438ac6425
Yasin Sutoglu <ysutoglu@gmail.com>
2023-02-01 06:58:57 +0300

    updating user settings done

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,14 @@
 import '@babel/polyfill'
 import { displayMap } from './mapbox';
 import { login,logout } from './login';
+import {updateSettings} from './updateSettings'
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout')
+const userDataForm= document.querySelector('.form-user-data')
+const userPasswordForm = document.querySelector('.form-user-settings');
 
 // DELEGATION
 if(mapBox){
@@ -26,3 +29,27 @@ if(loginForm){
 
 
 if(logOutBtn) logOutBtn.addEventListener('click', logout)
+
+if(userDataForm) userDataForm.addEventListener('submit' , async e=>{
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    await updateSettings({name,email} , 'data')
+})
+
+if(userPasswordForm) userPasswordForm.addEventListener('submit' , async e=>{
+    e.preventDefault()
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+   await updateSettings({ passwordCurrent, passwordConfirm,password }, 'password');
+
+   document.querySelector('.btn--save-password').textContent = 'Save Password';
+
+   document.getElementById('password-current').value='';
+   document.getElementById('password').value='';
+   document.getElementById('password-confirm').value = '';
+})
+
