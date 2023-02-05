aff524f4cbb142097a87c5a2d7bf96ce9f1764c3
Yasin Sutoglu <ysutoglu@gmail.com>
2023-02-05 13:23:42 +0300

    API done

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,5 @@
 const Tour = require('../models/tourModel'); //! DB model'i import ettik.
-const AppError = require('../utils/appError');
+const Booking = require('../models/bookingModel'); //! DB model'i import ettik.
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -12,7 +12,7 @@ exports.getCheckoutSession = catchAsync(async(req,res,next) =>{
     //2) create checkout session
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      success_url: `${req.protocol}://${req.get('host')}/`,
+      success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
       cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
       customer_email:req.user.email,
       client_reference_id: req.params.tourID,
@@ -26,3 +26,20 @@ exports.getCheckoutSession = catchAsync(async(req,res,next) =>{
     })
 
 })
+
+exports.createBookingCheckout = catchAsync(async (req,res,next)=>{
+    // This is only TEMPRORARY, because it's UNSECURE: everyone can make bookings without paying
+    const {tour, user, price} = req.query;
+
+    if(!tour && !user && !price) return next();
+
+    await Booking.create({tour, user, price});
+    
+    res.redirect(req.OriginalUrl.split('?')[0])
+});
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController')
 const authController = require('../controllers/authController')
+const bookingController = require('../controllers/bookingController')
+
 const router = express.Router();
 
 //? burada pug base template'i render ettirmek icin routing yaptık. render()'ın ikinci parametresi pug'a gonderecegimiz (object formatında) data'dır.
@@ -13,10 +15,12 @@ const router = express.Router();
 
 // router.use(authController.isLoggedIn)
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/',bookingController.createBookingCheckout ,authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
-router.get('/me',authController.protect,viewsController.getAccount);
+router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours',authController.protect,viewsController.getMyTours);
+
 
 router.post('/submit-user-data', authController.protect,viewsController.updateUserData)
 
